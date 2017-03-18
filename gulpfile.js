2cdc4978bded84069d532356b3308bab76dbc8ff
Eliot Easterling <eeasterl@kent.edu>
2017-03-18 17:17:18 -0400

    refactored gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
-/// <binding BeforeBuild='sassify' Clean='sassify' ProjectOpened='sassify' />
 "use strict";
 
-const gulp   = require("gulp"),
-      cssmin = require("gulp-cssmin"),
-      sass   = require("gulp-sass");
+const gulp   = require("gulp");
+const cssmin = require("gulp-cssmin");
+const sass   = require("gulp-sass");
 
-var root = "./Content/angular/";
-var paths = {
-    "sassy": root + "**/*.scss"
+var globs = {
+    "sass": "./src/**/*.scss"
 };
 
 gulp.task("sassify", function () {
-    return gulp.src(paths.sassy, { base: "./" })
+    return gulp.src(globs.sass, { base: "./" })
       .pipe(sass().on('error', sass.logError))
-      //.pipe(cssmin())
+      .pipe(cssmin())
       .pipe(gulp.dest("."));
 });
