3dba1599d9527a0848f111b8cf1940b083529944
quiddler <eliot@eliots-MacBook-Pro.local>
2017-04-02 16:54:52 -0400

    added css bundling to gulp file and added palette partial for easily changing colors

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,50 @@
 "use strict";
+ 
+// gulp & gulp plugins
+const gulp     = require("gulp");
+const less     = require("gulp-less");
+const sass     = require("gulp-sass");
+const cssmin   = require("gulp-cssmin");
+const concat   = require("gulp-concat");
+const srcMaps  = require("gulp-sourcemaps");
+const prefixer = require("gulp-autoprefixer");
+const merge    = require('merge-stream');
 
-const gulp   = require("gulp");
-const cssmin = require("gulp-cssmin");
-const sass   = require("gulp-sass");
-
-var globs = {
-    "sass": "./src/**/*.scss"
-};
-
-// transpiles scss files to minified css files in the same directory
+// where to send the bundled css file
+const cssDestination = "dist/styles";
+ 
 gulp.task("sassify", function () {
-    return gulp.src(globs.sass, { base: "./" })
-      .pipe(sass().on('error', sass.logError))
-      .pipe(cssmin())
-      .pipe(gulp.dest("."));
+
+    return gulp.src('src/styles/**/*.scss', { base: "./" })      
+        .pipe(sass().on('error', sass.logError))
+        .pipe(srcMaps.init())
+        .pipe(prefixer())
+        .pipe(cssmin())                 
+        .pipe(concat('styles.min.css'))
+        .pipe(srcMaps.write("."))
+        .pipe(gulp.dest(cssDestination));
+});
+ 
+gulp.task('bundle-css', function() {
+ 
+    var sassStream = gulp.src('src/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('sass-bundle.scss'));
+ 
+    var lessStream = gulp.src('src/styles/**/*.less')
+        .pipe(less())
+        .pipe(concat('less-bundle.less'));
+   
+    var cssStream = gulp.src('src/styles/**/*.css')
+        .pipe(concat('css-bundle.css'));
+ 
+    var mergedStream = merge(lessStream, sassStream, cssStream)
+        .pipe(concat('styles.min.css'))           // css bundle name
+        .pipe(srcMaps.init())
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(srcMaps.write("."))
+        .pipe(gulp.dest(cssDestination));
+ 
+    return mergedStream;
 });
