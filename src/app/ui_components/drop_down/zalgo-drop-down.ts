8a04add68b0217c1005ac4491e0292b29c0428ef
quiddler <eliot@eliots-MacBook-Pro.local>
2017-04-02 15:56:28 -0400

    added drop down files with code to work from

---
+++ src/app/ui_components/drop_down/zalgo-drop-down.ts
@@ -0,0 +1,39 @@
+﻿import { Component, AfterViewInit, OnDestroy, Input, ViewChild, HostListener } from "@angular/core";
+import { ResBus } from "../../bus/bus.service";
+import { ResAnimator } from "../../animations/res-animator";
+
+@Component({
+    moduleId:     module.id,
+    selector:    'zalgo-drop-down',
+    templateUrl: 'drop-down.html',
+    styleUrls:  ['drop-down.scss']
+})
+
+export class ZalgoDropDown implements AfterViewInit, OnDestroy {
+
+    @Input() links;
+    @Input() appName;
+
+    @ViewChild("resHeader") header;
+    @ViewChild("resHero") hero;
+
+    constructor(private bus: ResBus) {
+        this.registerPageLoad();
+    }
+
+    registerPageLoad() {
+        this.pageLoadToken = this.bus.on("page-load", (pageInfo) => {
+            this.toggleHeroText(pageInfo.heroTitle, pageInfo.heroTagline);
+        });
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.setOffset();
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    onscroll(event) {
+        this.navIsSticky = window.scrollY >= this.offset ? true : false;
+    }
+}
